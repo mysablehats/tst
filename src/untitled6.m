2ac28f355fbc414f851567ffcf4d1891a950d81e
mysablehats <frederico.klein@gmail.com>
2016-02-15 19:17:40 +0000

    added plot confusion

--- src/untitled6.m
+++ src/untitled6.m
@@ -5,5 +5,5 @@ for i = 1:length(data_val)
      [s1 s2 distances] = findTwoNearest(data_val(:,i),nodes); % for each data_val find nearest node
      class_val(i) = nodesl(s1);
 end
-confusion(y_val,class_val) % I am using the wrong names here. My Y is actually the T and the class_val is the y... I should change this everywhere, but for now
+plotconfusion(y_val,class_val) % I am using the wrong names here. My Y is actually the T and the class_val is the y... I should change this everywhere, but for now
      
