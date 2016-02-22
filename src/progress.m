42297b670d1a5bddfad717512854a779b2f61cdd
mysablehats <frederico.klein@gmail.com>
2016-02-22 18:32:29 +0000

    better progress function used by all and better way of displaying training set and validation set confusion matrices

--- src/progress.m
+++ src/progress.m
@@ -1,8 +1,13 @@
 function progress(i,maxi)
 onepercent  = maxi/100;
 if mod(i,onepercent)<.1
-    toc
-    disp(strcat('Progress: ',num2str(i/onepercent),'%'))
+    a = toc;
+    percentage = i/onepercent;
+    eta = (100-percentage)*a;
+    hours = fix(eta/3600);
+    minutes = fix((eta-hours*3600)/60);
+    seconds = eta-hours*3600-minutes*60;
+    disp(strcat('Progress: ',num2str(percentage),'% Estimated remaining time: ', num2str(hours), ':',num2str(minutes),':',num2str(seconds)))
     tic
 end
     
