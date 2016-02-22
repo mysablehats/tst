bfcfe83421b99eab351648527b385d93a72d94e6
mysablehats <frederico.klein@gmail.com>
2016-02-22 10:44:58 +0000

    prettier confusion plot

--- src/untitled6.m
+++ src/untitled6.m
@@ -2,6 +2,5 @@
 nodesl = labeling(nodes,data_train,y_train);
 class_val = labeling(data_val, nodes, nodesl); % puts labels based on nodes labels on validation data 
 class_train = labeling(data_train, nodes, nodesl); % puts labels based on nodes labels on training data 
-plotconfusion(y_val,class_val) % I am using the wrong names here. My Y is actually the T and the class_val is the y... I should change this everywhere, but for now
-figure
-plotconfusion(y_train,class_train)
+figure('Name','GWR classifier')
+plotconfusion(y_val,class_val, 'Performed on Validation Set:',y_train,class_train,'Performed on the Training Set:') % I am using the wrong names here. My Y is actually the T and the class_val is the y... I should change this everywhere, but for now
