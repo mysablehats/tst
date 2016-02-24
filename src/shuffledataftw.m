193b82bd8b5e44ebfd2c141ceda7582ad624aa10
mysablehats <frederico.klein@gmail.com>
2016-02-24 18:54:17 +0000

    horrible bug in shuffle data function

--- src/shuffledataftw.m
+++ src/shuffledataftw.m
@@ -6,5 +6,5 @@ dbgmsg('This is supposed to make training from the gas faster.',1)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 crazyI = randperm(length(y_train));
-shuffled_data_train = data_train(crazyI);
+shuffled_data_train = data_train(:,crazyI);
 shuffled_y_train = y_train(crazyI);
