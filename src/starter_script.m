f519d0813f6757c0125373ca6fdb9d67b90aa078
mysablehats <frederico.klein@gmail.com>
2016-02-25 16:31:33 +0000

    running most of the code in parallel now
    with shuffling done for each set (since initial parameters don’t seem
    to matter that much!)

--- src/starter_script.m
+++ src/starter_script.m
@@ -2,6 +2,7 @@ global VERBOSE
 VERBOSE = true;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%MESSAGES PART
+disp('####### ATTENTION IMBECILE: ####### YOU SHOULD ADD EVERYTHING TO THE PATH AND EXECUTE IT IN THE TST/SRC DIRECTORY. IF YOU WANT TO MAKE YOUR ALGORITHM HARD TO THESE CHANGES, BE MY GUEST, OTHERWISE JUST DO IT EACH TIME YOU START MATLAB, OR THIS WILL NOT RUN!!!!')
 dbgmsg('Running starter script')
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -9,46 +10,38 @@ dbgmsg('Running starter script')
 % %%validation and training set
 % dbgmsg('Skeleton data (training and validation) generated.')
 
-%clear all
+clear all
 
-%load_skel_data
-%[data_train, data_val] = removehipbias(data_train, data_val);
-%[data_train, y_train] = shuffledataftw(data_train, y_train);
+load_skel_data
+[data_train, data_val] = removehipbias(data_train, data_val);
 NODES = [10 10 10];
 %NODES = fix(NODES/30);
-savestructure = struct();
+savestructure(length(NODES)) = struct('nodes',[], 'data_train',[], 'y_train',[], 'nodes_gwr',[], 'edges_gwr',[], 'nodes_gng',[], 'edges_gng',[], 'class_train_gwr',[], 'class_val_gwr',[], 'class_train_gng',[], 'class_val_gng',[]);
 
 dbgmsg('Starting parallel pool for GWR and GNG for nodes:',num2str(NODES),1)
 parfor i = 1:length(NODES)
+    [savestructure(i).data_train, savestructure(i).y_train] = shuffledataftw(data_train, y_train);
     num_of_nodes = NODES(i);
     savestructure(i).nodes = num_of_nodes;
     dbgmsg('Starting gwr for process:',num2str(i),1)
-    [savestructure(i).nodes_gwr, savestructure(i).edges_gwr, ~, ~] = gwr(data_train,num_of_nodes);
+    [savestructure(i).nodes_gwr, savestructure(i).edges_gwr, ~, ~] = gwr(savestructure(i).data_train,num_of_nodes);
     dbgmsg('Finished gwr for process:',num2str(i),1)
     dbgmsg('Starting gng for process:',num2str(i),1)
-    [savestructure(i).nodes_gng,savestructure(i).edges_gng, ~, ~] = gng_lax(data_train,num_of_nodes);
-    dbgmsg('Finished gnr for process:',num2str(i),1)
-    
+    [savestructure(i).nodes_gng,savestructure(i).edges_gng, ~, ~] = gng_lax(savestructure(i).data_train,num_of_nodes);
+    dbgmsg('Finished gng for process:',num2str(i),1)
+    dbgmsg('Applying GWR labels for process:',num2str(i),1)
+    [savestructure(i).class_train_gwr, savestructure(i).class_val_gwr] = untitled6(savestructure(i).nodes_gwr, savestructure(i).data_train,data_val, savestructure(i).y_train);
+    dbgmsg('Applying GNG labels for process:',num2str(i),1)
+    [savestructure(i).class_train_gng, savestructure(i).class_val_gng] = untitled6(savestructure(i).nodes_gng, savestructure(i).data_train,data_val, savestructure(i).y_train);
 end
-save('../share/gng_gwr','data_train', 'data_val', 'y_train', 'y_val','savestructure')
-%%%%%%%%%%%%%%%%%%%%%%%%%%
-load('../share/gng_gwr')
-%%%%%%%dbgmsg('Starting parallel pool for labelling GWR and GNG nodes:',num2str(NODES),1)
-%doesnt work with parfor. don't know why, maybe should debug in the future.
-%but labelling isn't that time consuming
-for i = 1:length(savestructure)
-    num_of_nodes = savestructure(i).nodes;
-    [savestructure(i).class_train_gwr, savestructure(i).class_val_gwr] = untitled6(savestructure(i).nodes_gwr, data_train,data_val, y_train);
-    [savestructure(i).class_train_gng, savestructure(i).class_val_gng] = untitled6(savestructure(i).nodes_gng, data_train,data_val, y_train);
-end
-%figure
+
 %plotconfusion(ones(size(y_val)),y_val, 'always a fall on Validation Set:',zeros(size(y_val)),y_val, 'never a fall on Validation Set:')
 dbgmsg('Displaying multiple confusion matrices for GWR and GNG for nodes:',num2str(NODES),1)
 u = {};
 for i=1:length(savestructure)
     
-    gwr_u = {y_val,savestructure(i).class_val_gwr, strcat('GWR Val ', num2str(savestructure(i).nodes)),y_train,savestructure(i).class_train_gwr,strcat('GWR train ', num2str(savestructure(i).nodes))};
-    gng_u = {y_val,savestructure(i).class_val_gng, strcat('GNG Val ', num2str(savestructure(i).nodes)),y_train,savestructure(i).class_train_gng,strcat('GNG train ', num2str(savestructure(i).nodes))};
+    gwr_u = {y_val,savestructure(i).class_val_gwr, strcat('GWR Val ', num2str(savestructure(i).nodes)),savestructure(i).y_train,savestructure(i).class_train_gwr,strcat('GWR Train ', num2str(savestructure(i).nodes))};
+    gng_u = {y_val,savestructure(i).class_val_gng, strcat('GNG Val ', num2str(savestructure(i).nodes)),savestructure(i).y_train,savestructure(i).class_train_gng,strcat('GNG Train ', num2str(savestructure(i).nodes))};
     
     u = {u{:}, gwr_u{:}, gng_u{:}}; 
 end
