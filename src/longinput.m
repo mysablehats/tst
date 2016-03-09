b0403477ee89660436fe822aa43861a4d2194589
mysablehats <frederico.klein@gmail.com>
2016-03-09 21:08:46 +0000

    improved long input function,
    it avoids errors, but perhaps the errors it was calling mean that my

--- src/longinput.m
+++ src/longinput.m
@@ -3,20 +3,34 @@ newends = zeros(size(ends));
 linput = zeros(size(shortinput,1)*q,size(shortinput,2)-size(ends,2)*(q-1));
 %%% I will try with pre allocation if it doesnt work, then I will try with
 %%% the cat approach -> start from 2 and cat it...
+
+%there should be some error checking here, so if any "ends" is smaller than
+%q then, it should complain, but I won't write it because it is 
+
+newends = ends-(q-1);
 k = 1;
 dimshort = size(shortinput,1);
-lastk = 0;
-for j = 1:size(ends,2)
-    maxi = (ends(j)-(q-1));
-    for i = 1:maxi
-        a = zeros(dimshort*q,1);
-        for l =1:q
-            a(1+dimshort*(l-1):dimshort*l) = shortinput(:,k+l-1+(q-1)*(j-1));
+maxj = size(ends,2);
+endofdata = size(shortindex,2);
+for j = 1:maxj
+    maxi = newends(j);
+    if maxi<0
+        newends(j) =0;
+    else
+        for i = 1:maxi
+            a = zeros(dimshort*q,1);
+            for l =1:q
+                funnyindex = k+l-1+(q-1)*(j-1); %it should have the property of jumping the last q-1 points of a set
+                if funnyindex>endofdata
+                    newends(j) = newends(j)-1; %perhaps I cannot fill a whole vector? this should never happen, but I don't know, these indexes are funny/.
+                    break
+                else
+                    a(1+dimshort*(l-1):dimshort*l) = shortinput(:,funnyindex);
+                end
+            end
+            linput(:,k) = a;
+            k=k+1;
         end
-        linput(:,k) = a;
-        k=k+1;
     end
-    newends(j) = k-1-lastk;
-    lastk = newends(j);
 end
 end
