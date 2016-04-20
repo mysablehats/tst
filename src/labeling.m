e99852ac3fe45479f64eaa2adcc64fded9a35ca2
mysablehats <frederico.klein@gmail.com>
2016-04-20 21:26:57 +0100

    stupid mistake that would only happen if executed by octave

--- src/labeling.m
+++ src/labeling.m
@@ -11,10 +11,11 @@ dbgmsg('Applying labels to the prototypical nodes.',1)
 %%%%%
 try
     [~,ni] = pdist2(data',nodes', 'euclidean', 'Smallest',1);
+    labels = y(:,ni);
 catch
-    [~, ni1 ]= labelling(nodes, data, y);
+    [labels, ~ ]= labelling(nodes, data, y);
 end
-labels = y(:,ni);
+
 
 end
 function [labels, ni1 ]= labelling(nodes, data, y)
