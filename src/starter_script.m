4dffd46c25e871c06f8ed545fe20f7f863cbfbb5
mysablehats <frederico.klein@gmail.com>
2016-05-17 19:59:05 +0100

    point removal for the masses!
    and by that I mean of course the validation set

--- src/starter_script.m
+++ src/starter_script.m
@@ -5,7 +5,7 @@ env = aa_environment; % load environment variables
 %creates a structure with the results of different trials
 env.cstfilename=strcat(env.wheretosavestuff,env.SLASH,'cst.mat');
 if exist(env.cstfilename,'file')
-    load(env.cstfilename,'cst')
+    load(env.cstfilename,'simvar')
 end
 
 if ~exist('simvar','var')
@@ -71,9 +71,9 @@ simvar(end).datainputvectorsize = size(data.train.data,1);
 
 % set other additional simulation variables
 simvar(end).TEST = 0; % set to false to actually run it
-simvar(end).PARA = 0;
-simvar(end).P = 1;
-simvar(end).NODES_VECT = [1000];
+simvar(end).PARA = 1;
+simvar(end).P = 4;
+simvar(end).NODES_VECT = [100 1000];
 simvar(end).MAX_EPOCHS_VECT = [10];
 simvar(end).ARCH_VECT = [1];
 simvar(end).MAX_NUM_TRIALS = 1;
@@ -199,7 +199,6 @@ for architectures = simvar(end).ARCH_VECT
 end
 simvar(end) = [];
 end
-
 function allconn = allconnset(n, params)
 allconn_set = {...
     {... %%%% ARCHITECTURE 1
@@ -276,22 +275,12 @@ pallconn{1,1}{1,6} = paramsZ; % I only change the initial points of the position
 
 %[a.sv, a.mt] = starter_sc(data, pallconn, 1);
 
-[~, a.mt] = starter_sc(data, pallconn, 1);
+[~, a.mt] = starter_sc(data, pallconn);
 % confconf = struct('val','val', 'train', '')
 % a.mt(4,5) = struct('conffig', 'hello','confusions', confconf,'conffvig', 'hello');
 
 end
-function [savestructure, metrics] = starter_sc(data, allconn, P)
-
-PLOTIT = true;
-data_val = data.val.data;
-data_train = data.train.data;
-y_val = data.val.y;
-y_train = data.train.y;
-ends_train = data.train.ends;
-ends_val = data.val.ends;
-
-
+function [savestructure, metrics] = starter_sc(savestructure, allconn)
 %% starter_script
 % This is the main function to run the chained classifier, label and generate
 % confusion matrices and recall, precision and F1 values for the skeleton
@@ -327,14 +316,18 @@ end
 inputs = struct('input_clip',[],'input',[],'input_ends',[],'oldwhotokill',{}, 'index', {});
 gas_data = struct('name','','class',[],'y',[],'inputs',inputs,'bestmatch',[],'bestmatchbyindex',[],'whotokill',{});
 gas_methods(1:length(arq_connect)) = struct('name','','edges',[],'nodes',[],'fig',[],'nodesl',[]); %bestmatch will have the training matrix for subsequent layers
-vt_data = struct('indexes',[],'data',[],'ends',[],'gas',gas_data);
-savestructure(1:P) = struct('maxnodes',[], 'gas', gas_methods, 'train',vt_data,'val',vt_data,'figset',[]); % I have a problem with figset. I don't kno
+savestructure.gas = gas_methods;
+savestructure.train.indexes = [];
+savestructure.train.gas = gas_data;
+savestructure.val.indexes = [];
+savestructure.val.gas = gas_data;
+
 for i = 1:length(savestructure) % oh, I don't know how to do it elegantly
-    savestructure(i).figset = {};
+    savestructure.figset = {};
 end
 
 %%% end of gas structures region
-
+PLOTIT = true;
 
 %% Gas-chain Classifier
 % This part executes the chain of interlinked gases. Each iteration is one
@@ -345,34 +338,25 @@ end
 % 3. Generate matrix of best matching units to be used by the next gas
 % architecture
 
-dbgmsg('Starting chain structure for GWR and GNG for nodes:',num2str(P),1)
+dbgmsg('Starting chain structure for GWR and GNG for nodes:',num2str(labindex),1)
 dbgmsg('###Using multilayer GWR and GNG ###',1)
 
-for i = 1:P
-    %[savestructure(i).train.data, savestructure(i).train.indexes] =
-    %shuffledataftw(data_train); % I cant shuffle any longer...
-    %but I still need to assign it !
-    savestructure(i).train.data = data_train;
-    savestructure(i).train.ends = ends_train;
-    savestructure(i).train.y = y_train;
-    
-    for j = 1:length(arq_connect)
-        savestructure(i) = gas_method(savestructure(i), arq_connect(j),j, size(data_train,1)); % I had to separate it to debug it.
-        metrics(i,j).outparams = savestructure(i).gas(j).outparams;
-    end
-    
+for j = 1:length(arq_connect)
+    [savestructure, savestructure.train] = gas_method(savestructure, savestructure.train,'train', arq_connect(j),j, size(savestructure.train.data,1)); % I had to separate it to debug it.
+    metrics(j).outparams = savestructure.gas(j).outparams;
+    [savestructure, savestructure.val ]= gas_method(savestructure, savestructure.val,'val', arq_connect(j),j, size(savestructure.train.data,1));
 end
+
+
 %% Gas Outcomes
 % This should be made into a function... It is a nice graph to perhaps
 % debug the gas...
-if 1% PLOTIT
-    for i = 1:length(savestructure)
-        figure
-        for j = 1:length(arq_connect)
-            subplot (1,length(arq_connect),j)
-            hist(savestructure(i).gas(j).outparams.graph.errorvect)
-            title((savestructure(i).gas(j).name))
-        end
+if PLOTIT
+    figure
+    for j = 1:length(arq_connect)
+        subplot (1,length(arq_connect),j)
+        hist(savestructure.gas(j).outparams.graph.errorvect)
+        title((savestructure.gas(j).name))
     end
 end
 %% Labelling
@@ -385,113 +369,74 @@ end
 % closest point and assigns to that. In a sense the gas can be seen as a
 % dimensional (as opposed to temporal) filter, encoding prototypical
 % action-lets.
-dbgmsg('Labelling',num2str(P),1)
+dbgmsg('Labelling',num2str(labindex),1)
 
-whatIlabel = 1:length(savestructure(1).gas); %change this series for only the last value to label only the last gas
+whatIlabel = 1:length(savestructure.gas); %change this series for only the last value to label only the last gas
 
-for i=1:length(savestructure)
-    savestructure(i).val.data = data_val;
-    savestructure(i).val.ends = ends_val;
-    savestructure(i).val.y = y_val;
-    %%
-    % Concatenating the inputs to be labelled (has to be done on all layers:
-    for j =  1:length(arq_connect)
-        %labeling
-        %pretty much useless unless it is the last layer, but I can label
-        %everyone, so I will.
-        savestructure(i).train.gas(j).name = arq_connect(j).name;
-        savestructure(i).val.gas(j).name = arq_connect(j).name;
-        
-        dbgmsg('Setting validation input (and clipping output) for gas: ''',savestructure(i).gas(j).name,''' (', num2str(j),') for process:',num2str(i),1)
-        [~, savestructure(i).val.gas(j).inputs.input, savestructure(i).val.gas(j).inputs.input_ends, savestructure(i).val.gas(j).y, ~, savestructure.val.gas(j).inputs.index, savestructure.val.gas(j).inputs.awk]  = setinput(arq_connect(j), savestructure(i), size(data_train,1), savestructure(i).val);
-        
-        % I didn't realize, but I need to do this for the validation
-        % dataset as well.
-        dbgmsg('Finding best matching units for gas: ''',savestructure(i).gas(j).name,''' (', num2str(j),') for process:',num2str(i),1)
-        [~, savestructure(i).val.gas(j).bestmatchbyindex] = genbestmmatrix(savestructure(i).gas(j).nodes, savestructure(i).val.gas(j).inputs.input, arq_connect(j).layertype, arq_connect(j).q); %assuming the best matching node always comes from initial dataset!
-    end
-    %%
-    % Specific part on what I want to label
-    for j = whatIlabel
-        dbgmsg('Applying labels for gas: ''',savestructure(i).gas(j).name,''' (', num2str(j),') for process:',num2str(i),1)
-        [savestructure(i).train.gas(j).class, savestructure(i).val.gas(j).class,savestructure(i).gas(j).nodesl ] = labeller(savestructure(i).gas(j).nodes, savestructure(i).train.gas(j).bestmatchbyindex,  savestructure(i).val.gas(j).bestmatchbyindex, savestructure(i).train.gas(j).inputs.input, savestructure(i).train.gas(j).y);
-        %%%% I dont understand what I did, so I will code this again.
-        %%% why did I write .bestmatch when it should be nodes??? what was I thinnking [savestructure(i).train.gas(j).class, savestructure(i).val.gas(j).class] = untitled6(savestructure(i).gas(j).bestmatch, savestructure(i).train.gas(j).inputs.input,savestructure(i).val.gas(j).inputs.input, y_train);
-        
-    end
+%%
+% Specific part on what I want to label
+for j = whatIlabel
+    dbgmsg('Applying labels for gas: ''',savestructure.gas(j).name,''' (', num2str(j),') for process:',num2str(i),1)
+    [savestructure.train.gas(j).class, savestructure.val.gas(j).class,savestructure.gas(j).nodesl ] = labeller(savestructure.gas(j).nodes, savestructure.train.gas(j).bestmatchbyindex,  savestructure.val.gas(j).bestmatchbyindex, savestructure.train.gas(j).inputs.input, savestructure.train.gas(j).y);
+    %%%% I dont understand what I did, so I will code this again.
+    %%% why did I write .bestmatch when it should be nodes??? what was I thinnking [savestructure.train.gas(j).class, savestructure.val.gas(j).class] = untitled6(savestructure.gas(j).bestmatch, savestructure.train.gas(j).inputs.input,savestructure.val.gas(j).inputs.input, y_train);
+    
 end
 
 %% Displaying multiple confusion matrices for GWR and GNG for nodes
 % This part creates the matrices that can later be shown with the
 % plotconfusion() function.
-for i =1:length(savestructure)
-    savestructure(i).figset = {}; %% you should clear the set first if you want to rebuild them
-    dbgmsg('Displaying multiple confusion matrices for GWR and GNG for nodes:',num2str(P),1)
-end
-for i=1:length(savestructure)
-    for j = whatIlabel
-        [~,metrics(i,j).confusions.val,~,~] = confusion(savestructure(i).val.gas(j).y,savestructure(i).val.gas(j).class);
-        [~,metrics(i,j).confusions.train,~,~] = confusion(savestructure(i).train.gas(j).y,savestructure(i).train.gas(j).class);
-        
-        dbgmsg(num2str(i),'-th set.',savestructure(i).gas(j).name,' Confusion matrix on this validation set:',writedownmatrix(metrics(i,j).confusions.val),1)
-        savestructure(i).gas(j).fig.val =   {savestructure(i).val.gas(j).y,     savestructure(i).val.gas(j).class,  strcat(savestructure(i).gas(j).name,savestructure(i).gas(j).method,'V', num2str(savestructure(i).maxnodes))};
-        savestructure(i).gas(j).fig.train = {savestructure(i).train.gas(j).y,   savestructure(i).train.gas(j).class,strcat(savestructure(i).gas(j).name,savestructure(i).gas(j).method,'T', num2str(savestructure(i).maxnodes))};
-        %savestructure(i).figset = [savestructure(i).figset, savestructure(i).gas(j).fig.val, savestructure(i).gas(j).fig.train];
-        %%%
-        metrics(i,j).conffig = savestructure(i).gas(j).fig;
-    end
+
+savestructure.figset = {}; %% you should clear the set first if you want to rebuild them
+dbgmsg('Displaying multiple confusion matrices for GWR and GNG for nodes:',num2str(labindex),1)
+
+for j = whatIlabel
+    [~,metrics(j).confusions.val,~,~] = confusion(savestructure.val.gas(j).y,savestructure.val.gas(j).class);
+    [~,metrics(j).confusions.train,~,~] = confusion(savestructure.train.gas(j).y,savestructure.train.gas(j).class);
+    
+    dbgmsg(savestructure.gas(j).name,' Confusion matrix on this validation set:',writedownmatrix(metrics(j).confusions.val),1)
+    savestructure.gas(j).fig.val =   {savestructure.val.gas(j).y,     savestructure.val.gas(j).class,  strcat(savestructure.gas(j).name,savestructure.gas(j).method,'V')};
+    savestructure.gas(j).fig.train = {savestructure.train.gas(j).y,   savestructure.train.gas(j).class,strcat(savestructure.gas(j).name,savestructure.gas(j).method,'T')};
+    %savestructure.figset = [savestructure.figset, savestructure.gas(j).fig.val, savestructure.gas(j).fig.train];
+    %%%
+    metrics(j).conffig = savestructure.gas(j).fig;
 end
 
 %% Actual display of the confusion matrices:
 if PLOTIT
-    for i = 1:length(savestructure)
-        figure
-        plotconf([metrics(i,:)])
-        %plotconf(savestructure(i).figset{:})
-        figure
-        plotconfusion(savestructure(i).gas(end).fig.val{:})
-    end
+    figure
+    plotconf([metrics(:)])
+    %plotconf(savestructure.figset{:})
+    figure
+    plotconfusion(savestructure.gas(end).fig.val{:})
 end
-% plotconfusion(ones(size(y_val)),y_val, 'always guess "it''s a fall" on Validation Set:',zeros(size(y_val)),y_val, 'always guess "it''s NOT a fall" on Validation Set:')
-% clear i
-% %% Calculate my own measures over the matrices
-% % TO DO: This is bad. I should probably read the confusion documentation instead of doing this
-% % manually
-% for j = whatIlabel %this is weird, but I just changed this to show only the last gas
-%     metrics = howgood(savestructure,j);
-%     dbgmsg(savestructure(1).gas(j).name,'\t [All data for Validation set] Sensitivity/Recall overall mean:\t', num2str(metrics(1)),'%%\t|| Specificity overall mean:\t', num2str(metrics(2)),'%%\t|| Precision overall mean:\t', num2str(metrics(3)),'%%\t|| F1 overall mean:\t', num2str(metrics(4)),'%%',1)
-%     metrics = whoisbest(savestructure,j);
-%     dbgmsg(savestructure(1).gas(j).name,'\t [All data for Validation set] Best Sensitivity/Recall:        \t', num2str(metrics(1)),'%%\t|| Best Specificity:        \t', num2str(metrics(2)),'%%\t|| Best Precision:        \t', num2str(metrics(3)),'%%\t|| Best F1:       \t', num2str(metrics(4)),'%%',1)
-% end
 end
-function savestructure = gas_method(savestructure, arq_connect,j, dimdim)
+function [sst, sstv] = gas_method(sst, sstv, vot, arq_connect,j, dimdim)
 %% Gas Method
 % This is a function to go over a gas of the classifier, populate it with the apropriate input and generate the best matching units for the next layer.
 %% Setting up some labels
-savestructure.gas(j).name = arq_connect.name;
-savestructure.gas(j).method = arq_connect.method;
-savestructure.gas(j).layertype = arq_connect.layertype;
+sst.gas(j).name = arq_connect.name;
+sst.gas(j).method = arq_connect.method;
+sst.gas(j).layertype = arq_connect.layertype;
 arq_connect.params.layertype = arq_connect.layertype;
 
 %% Choosing the right input for this layer
 % This calls the function set input that chooses what will be written on the .inputs variable. It also handles the sliding window concatenations and saves the .input_ends properties, so that this can be done recursevely.
 % After some consideration, I have decided that all of the long inputing
 % will be done inside setinput, because it it would be easier.
+dbgmsg('Working on gas: ''',sst.gas(j).name,''' (', num2str(j),') with method: ',sst.gas(j).method ,' for process:',num2str(labindex),1)
 
-[savestructure.train.gas(j).inputs.input_clip, savestructure.train.gas(j).inputs.input, savestructure.train.gas(j).inputs.input_ends, savestructure.train.gas(j).y, savestructure.train.gas(j).inputs.oldwhotokill, savestructure.train.gas(j).inputs.index, savestructure.train.gas(j).inputs.awk ]  = setinput(arq_connect, savestructure, dimdim, savestructure.train); %%%%%%
+[sstv.gas(j).inputs.input_clip, sstv.gas(j).inputs.input, sstv.gas(j).inputs.input_ends, sstv.gas(j).y, sstv.gas(j).inputs.oldwhotokill, sstv.gas(j).inputs.index, sstv.gas(j).inputs.awk ]  = setinput(arq_connect, sst, dimdim, sstv); %%%%%%
 
 %%
 % After setting the input, we can actually run the gas, either a GNG or the
 % GWR function we wrote.
-%%%% PRE-MESSAGE
-dbgmsg('Working on gas: ''',savestructure.gas(j).name,''' (', num2str(j),') with method: ',savestructure.gas(j).method ,' for process:',num2str(labindex),1)
+if strcmp(vot, 'train')
 %DO GNG OR GWR
-
-[savestructure.gas(j).nodes, savestructure.gas(j).edges, savestructure.gas(j).outparams] = gas_wrapper(savestructure.train.gas(j).inputs.input_clip,arq_connect);
-
+[sst.gas(j).nodes, sst.gas(j).edges, sst.gas(j).outparams] = gas_wrapper(sstv.gas(j).inputs.input_clip,arq_connect);
+end
 %%%% POS-MESSAGE
-dbgmsg('Finished working on gas: ''',savestructure.gas(j).name,''' (', num2str(j),') with method: ',savestructure.gas(j).method ,'.Num of nodes reached:',num2str(savestructure.gas(j).outparams.graph.nodesvect(end)),' for process:',num2str(labindex),1)
-%%%% FIND BESTMATCHING UNITS
+dbgmsg('Finished working on gas: ''',sst.gas(j).name,''' (', num2str(j),') with method: ',sst.gas(j).method ,'.Num of nodes reached:',num2str(sst.gas(j).outparams.graph.nodesvect(end)),' for process:',num2str(labindex),1)
 
 %% Best-matching units
 % The last part is actually finding the best matching units for the gas.
@@ -504,17 +449,17 @@ dbgmsg('Finished working on gas: ''',savestructure.gas(j).name,''' (', num2str(j
 % Well, for the last gas it does make a difference, since these units will
 % not be used... Still I will  not fix it unless I have to.
 %PRE MESSAGE
-dbgmsg('Finding best matching units for gas: ''',savestructure.gas(j).name,''' (', num2str(j),') for process:',num2str(labindex),1)
-[~, savestructure.train.gas(j).bestmatchbyindex] = genbestmmatrix(savestructure.gas(j).nodes, savestructure.train.gas(j).inputs.input, arq_connect.layertype, arq_connect.q); %assuming the best matching node always comes from initial dataset!
+dbgmsg('Finding best matching units for gas: ''',sst.gas(j).name,''' (', num2str(j),') for process:',num2str(labindex),1)
+[~, sstv.gas(j).bestmatchbyindex] = genbestmmatrix(sst.gas(j).nodes, sstv.gas(j).inputs.input, arq_connect.layertype, arq_connect.q); %assuming the best matching node always comes from initial dataset!
 
 %% Post-conditioning function
 %This will be the noise removing function. I want this to be optional or allow other things to be done to the data and I
 %am still thinking about how to do it. Right now I will just create the
 %whattokill property and let setinput deal with it.
 if arq_connect.params.removepoints
-    dbgmsg('Flagging noisy input for removal from gas: ''',savestructure.gas(j).name,''' (', num2str(j),') with points with more than',num2str(arq_connect.params.gamma),' standard deviations, for process:',num2str(labindex),1)
-    savestructure.train.gas(j).whotokill = removenoise(savestructure.gas(j).nodes, savestructure.train.gas(j).inputs.input, savestructure.train.gas(j).inputs.oldwhotokill, arq_connect.params.gamma, savestructure.train.gas(j).inputs.index);
+    dbgmsg('Flagging noisy input for removal from gas: ''',sst.gas(j).name,''' (', num2str(j),') with points with more than',num2str(arq_connect.params.gamma),' standard deviations, for process:',num2str(labindex),1)
+    sstv.gas(j).whotokill = removenoise(sst.gas(j).nodes, sstv.gas(j).inputs.input, sstv.gas(j).inputs.oldwhotokill, arq_connect.params.gamma, sstv.gas(j).inputs.index);
 else
-    dbgmsg('Skipping removal of noisy input for gas:',savestructure.gas(j).name)
+    dbgmsg('Skipping removal of noisy input for gas:',sst.gas(j).name)
 end
 end
